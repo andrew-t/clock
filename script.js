8d4d827183de0dd96eb6d4b88f39d99fb2fc0fc1
Andrew <git@andrewt.net>
2021-11-03 21:17:58 +0000

    display hour at midnight

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { cIf } from './util.js';
 setInterval(() => {
 	const d = new Date();
 	cIf(document.getElementById('main'), 'am', d.getHours() < 12);
-	document.getElementById('hour').set(d.getHours() % 12);
+	document.getElementById('hour').set((d.getHours() + 12) % 12);
 	document.getElementById('minute-tens').set(d.getMinutes() / 10);
 	document.getElementById('minute-ones').set(d.getMinutes() % 10);
 	document.getElementById('second-tens').set(d.getSeconds() / 10);
